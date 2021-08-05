8aa9063a262f7e7e5a4dac75c3318b63cadf1700
Markus Hinsche <mhinsche@childgrowthmonitor.org>
2021-08-05 12:35:10 +0200

    Train with resize

--- src/models/CNNDepthMap/CNNDepthMap-height/q3-depthmap-plaincnn-height/src/config.py
+++ src/models/CNNDepthMap/CNNDepthMap-height/q3-depthmap-plaincnn-height/src/config.py
@@ -11,7 +11,7 @@ CONFIG = Bunch(dict(
     IMAGE_TARGET_HEIGHT=240,
     IMAGE_TARGET_WIDTH=180,
     EPOCHS=1000,
-    BATCH_SIZE=256,
+    BATCH_SIZE=128,
     SHUFFLE_BUFFER_SIZE=2560,
     NORMALIZATION_VALUE=7.5,
     LEARNING_RATE=0.0007,
@@ -19,7 +19,7 @@ CONFIG = Bunch(dict(
     USE_DROPOUT=False,
     USE_WANDB=False,
     USE_MULTIGPU=False,
-    CLUSTER_NAME='gpu-cluster',
+    CLUSTER_NAME='gpu-cluster-M60',
 
     # Parameters for dataset generation.
     TARGET_INDEXES=[0],  # 0 is height, 1 is weight.
