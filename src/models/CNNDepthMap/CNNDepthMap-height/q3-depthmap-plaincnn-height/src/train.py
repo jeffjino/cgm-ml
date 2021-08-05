fc5c5e627c616a8e18c870dd550a9f017b3a5624
Markus Hinsche <mhinsche@childgrowthmonitor.org>
2021-08-05 15:24:56 +0200

    Rename temp_common to common, works on cluster

--- src/models/CNNDepthMap/CNNDepthMap-height/q3-depthmap-plaincnn-height/src/train.py
+++ src/models/CNNDepthMap/CNNDepthMap-height/q3-depthmap-plaincnn-height/src/train.py
@@ -28,13 +28,13 @@ run = Run.get_context()
 if run.id.startswith("OfflineRun"):
     # Copy common into the temp folder
     common_dir_path = REPO_DIR / "src/common"
-    temp_common_dir = Path(__file__).parent / "temp_common"
+    temp_common_dir = Path(__file__).parent / "common"
     copy_dir(src=common_dir_path, tgt=temp_common_dir, glob_pattern='*/*.py', should_touch_init=True)
 
-from temp_common.model_utils.model_plaincnn import create_cnn  # noqa: E402
-from temp_common.model_utils.preprocessing import (  # noqa: E402
+from common.model_utils.model_plaincnn import create_cnn  # noqa: E402
+from common.model_utils.preprocessing import (  # noqa: E402
     filter_blacklisted_qrcodes, preprocess_depthmap, preprocess_targets)
-from temp_common.model_utils.utils import (  # noqa: E402
+from common.model_utils.utils import (  # noqa: E402
     download_dataset, get_dataset_path, AzureLogCallback,
     create_tensorboard_callback, get_optimizer, setup_wandb)
 
