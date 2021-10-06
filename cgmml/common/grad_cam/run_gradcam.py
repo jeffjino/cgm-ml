cb09cef83034e84f56a8f14026101f65da6d7409
jasminziegler <ziegler-jasmin@outlook.de>
2021-10-06 13:46:12 +0200

    extracted many funtions to run_gradcam.py script, enabled for multi-depthmap processing, stuck on integrating in get_height_pred

--- cgmml/common/grad_cam/run_gradcam.py
+++ cgmml/common/grad_cam/run_gradcam.py
@@ -34,6 +34,61 @@ from cgmml.common.evaluation.eval_utilities import download_model
 REPO_DIR = Path(os.getcwd()).parents[2].absolute()
 
 
+def show_depthmaps(depthmaps):
+    for depthmap in depthmaps:
+        plt.imshow(depthmap)
+        plt.show()
+
+
+#process multiple
+def make_gradcam_heatmaps(preprocessed_depthmaps, model, last_conv_layer_name, pred_index=None):
+    # First, we create a model that maps the input image to the activations
+    # of the last conv layer as well as the output predictions
+    grad_model = tf.keras.models.Model(
+        [model.inputs], [model.get_layer(last_conv_layer_name).output, model.output]
+    )
+
+    # Then, we compute the gradient of the top predicted class for our input image
+    # with respect to the activations of the last conv layer
+    heatmaps = []
+    for depthmap in preprocessed_depthmaps:
+        with tf.GradientTape() as tape:
+            last_conv_layer_output, preds = grad_model(depthmap)
+            if pred_index is None:
+                pred_index = tf.argmax(preds[0])
+            class_channel = preds[:, pred_index]
+
+        # This is the gradient of the output neuron (top predicted or chosen)
+        # with regard to the output feature map of the last conv layer
+        grads = tape.gradient(class_channel, last_conv_layer_output)
+
+        # This is a vector where each entry is the mean intensity of the gradient
+        # over a specific feature map channel
+        pooled_grads = tf.reduce_mean(grads, axis=(0, 1, 2))
+
+        # We multiply each channel in the feature map array
+        # by "how important this channel is" with regard to the top predicted class
+        # then sum all the channels to obtain the heatmap class activation
+        last_conv_layer_output = last_conv_layer_output[0]
+        heatmap = last_conv_layer_output @ pooled_grads[..., tf.newaxis]
+        heatmap = tf.squeeze(heatmap)
+
+        # For visualization purpose, we will also normalize the heatmap between 0 & 1
+        heatmap = tf.maximum(heatmap, 0) / tf.math.reduce_max(heatmap)
+        heatmap = heatmap.numpy()
+        heatmaps.append(heatmap)
+        #plt.matshow(heatmap)
+        #plt.show()
+    return heatmaps
+
+
+def show_heatmaps(heatmaps):
+    for heatmap in heatmaps:
+        plt.matshow(heatmap)
+        plt.show()
+
+
+#process one
 def make_gradcam_heatmap(img_array, model, last_conv_layer_name, pred_index=None):
     # First, we create a model that maps the input image to the activations
     # of the last conv layer as well as the output predictions
@@ -71,69 +126,99 @@ def make_gradcam_heatmap(img_array, model, last_conv_layer_name, pred_index=None
     return heatmap.numpy()
 
 
-def save_and_display_gradcam(dmap_array, heatmap, cam_path, alpha=0.4):
+#process multiple
+def return_gradcams(preprocessed_depthmaps, heatmaps, transparency):
+    superimposed_imgs = []
+    i = 0
+    for heatmap in heatmaps:
+        # Rescale heatmap to a range 0-255
+        heatmap = np.uint8(255 * heatmap)
+        jet_cm = cm.get_cmap("jet")
+
+        # Use RGB values of the colormap
+        jet_colors = jet_cm(np.arange(256))[:, :3]
+        jet_heatmap = jet_colors[heatmap]
+
+        # Create an image with RGB colorized heatmap
+        jet_heatmap = keras.preprocessing.image.array_to_img(jet_heatmap)
+        #resize heatmap to size of depthmap
+        jet_heatmap = jet_heatmap.resize((preprocessed_depthmaps[i].shape[1], preprocessed_depthmaps[i].shape[0]))
+
+        jet_heatmap = keras.preprocessing.image.img_to_array(jet_heatmap)
+        # Superimpose the heatmap on original image
+        ## TODO - fix dimensions!! how do i do this later with batches - for now: strip first batch dim
+        superimposed_img = jet_heatmap * transparency + preprocessed_depthmaps[i]
+        superimposed_img = keras.preprocessing.image.array_to_img(superimposed_img)
+
+        superimposed_imgs.append(superimposed_img)
+        #plt.imshow(superimposed_img)
+        #plt.show()
+        i += 1
+
+    return superimposed_imgs
+
+
+def remove_batchdim(depthmaps):
+    new_depthmaps = []
+    for depthmap in depthmaps:
+        depthmap = depthmap[0, :, :, :]
+        new_depthmaps.append(depthmap)
     
-    #dmap_array = dmap.depthmap_arr.reshape(1, 240, 180, 1)
-    print(dmap_array.shape)
-    
-    # Rescale heatmap to a range 0-255
-    heatmap = np.uint8(255 * heatmap)
+    return new_depthmaps
 
-    # Use jet colormap to colorize heatmap
-    jet = cm.get_cmap("jet")
+def add_batchdim(depthmaps):
+    new_depthmaps = []
+    for depthmap in depthmaps:
+        depthmap = np.expand_dims(depthmap, axis=0)
+        new_depthmaps.append(depthmap)
 
-    # Use RGB values of the colormap
-    jet_colors = jet(np.arange(256))[:, :3]
-    jet_heatmap = jet_colors[heatmap]
-
-    # Create an image with RGB colorized heatmap
-    jet_heatmap = keras.preprocessing.image.array_to_img(jet_heatmap)
-    print("jet_heatmap.shape", jet_heatmap.size)
-    
-    jet_heatmap = jet_heatmap.resize((dmap_array.shape[2], dmap_array.shape[1]))
-    
-    print("jet_heatmap.shape", jet_heatmap.size)
-    jet_heatmap = keras.preprocessing.image.img_to_array(jet_heatmap)
-
-    # Superimpose the heatmap on original image
-    ## TODO - fix dimensions!! how do i do this later with batches - for now: strip first batch dim
-    dmap_array = dmap_array[0, :, :, :]
-    print(dmap_array.shape)
-    superimposed_img = jet_heatmap * alpha + dmap_array
-    superimposed_img = keras.preprocessing.image.array_to_img(superimposed_img)
-    print("superimposed_img.shape ", superimposed_img.size)
-
-    # Save the superimposed image
-    superimposed_img.save(cam_path)
-
-    # Display Grad CAM
-    display(Image(cam_path))
-    
-    plt.imshow(dmap_array)
-    plt.show()
+    return new_depthmaps
 
 
+def show_gradcam(superimposed_imgs):
+    for img in superimposed_imgs:
+        plt.matshow(img)
+        plt.show()
+
+
+#process one
 def return_gradcam(dmap_array, heatmap, transparency):
     # Rescale heatmap to a range 0-255
     heatmap = np.uint8(255 * heatmap)
+    print("heatmap.shape = ", heatmap.shape)
     jet_cm = cm.get_cmap("jet")
 
     # Use RGB values of the colormap
     jet_colors = jet_cm(np.arange(256))[:, :3]
     jet_heatmap = jet_colors[heatmap]
+    print("jet_heatmap.shape = ", jet_heatmap.shape)
 
     # Create an image with RGB colorized heatmap
     jet_heatmap = keras.preprocessing.image.array_to_img(jet_heatmap)
+    print("jet_heatmap.size = ", jet_heatmap.size)
 
     #resize heatmap to size of depthmap
     jet_heatmap = jet_heatmap.resize((dmap_array.shape[2], dmap_array.shape[1]))
+    print("jet_heatmap.size = ", jet_heatmap.size)
 
     jet_heatmap = keras.preprocessing.image.img_to_array(jet_heatmap)
+    print("jet_heatmap.shape = ", jet_heatmap.shape)
     # Superimpose the heatmap on original image
     ## TODO - fix dimensions!! how do i do this later with batches - for now: strip first batch dim
-    #dmap_array = dmap_array[0, :, :, :] do I need this in cgm-rg? idk
-    print(dmap_array.shape)
-    superimposed_img = jet_heatmap * alpha + dmap_array
+    dmap_array = dmap_array[0, :, :, :]  #do I need this in cgm-rg? idk
+    print("dmap_array.shape = ", dmap_array.shape)
+    superimposed_img = jet_heatmap * transparency + dmap_array
+    print("superimposed_img.shape ", superimposed_img.size)
+    plt.imshow(superimposed_img)
+    plt.show()
+    superimposed_img = keras.preprocessing.image.array_to_img(superimposed_img)
+    print("superimposed_img.shape ", superimposed_img.size)
+
+    #display(Image(superimposed_img))
+    #superimposed_img.show()
+    plt.imshow(superimposed_img)
+    plt.imshow(dmap_array)
+    plt.show()
 
     return superimposed_img
 
